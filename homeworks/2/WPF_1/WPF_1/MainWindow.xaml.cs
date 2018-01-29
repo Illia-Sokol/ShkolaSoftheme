cb6965a3ddf88d65d96af8b080e387dd05e3965e
Illia Sokol <illiasokolovskyi@gmail.com>
2018-01-29 08:15:28 +0200

    homework2 WPF

---
+++ homeworks/2/WPF_1/WPF_1/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Windows;
+using ConsoleApplication1;
+
+namespace WPF_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var myNewClass = new Class1();
+        }
+
+        private void OkClick(object sender, RoutedEventArgs e)
+          {
+            MessageBox.Show("HEllo " + userName.text);
+          }
+    }
+}
