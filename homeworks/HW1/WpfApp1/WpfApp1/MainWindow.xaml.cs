9343881eb5a890ee3f5ce5329a638da99f27bf8a
Illia Sokol <illiasokolovskyi@gmail.com>
2018-02-06 09:17:33 +0200

    create first WPF application

---
+++ homeworks/HW1/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowUserName(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("my name is " + userName.Text);
+        }
+    }
+}
--- homeworks/HW1/WpfApp1/WpfApp1/Properties/Settings.Designer.cs
+++ homeworks/HW1/WpfApp1/WpfApp1/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WPF_1.Properties
+namespace WpfApp1.Properties
 {
 
 
