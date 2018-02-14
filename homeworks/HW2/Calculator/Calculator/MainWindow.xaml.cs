f6206e85300922cf1b91beeb2f96d396ec68e2bf
Illia Sokol <illiasokolovskyi@gmail.com>
2018-02-14 10:32:53 +0200

    create Calculator layout

---
+++ homeworks/HW2/Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateClick(object sender, RoutedEventArgs e)
+        {
+            if (multiplication.IsChecked.HasValue && multiplication.IsChecked.Value)
+            {
+                Multiplication();
+            }
+            else if (division.IsChecked.HasValue && division.IsChecked.Value)
+            {
+                Division();
+            }
+            else if (addition.IsChecked.HasValue && addition.IsChecked.Value)
+            {
+                Addition();
+            }
+            else if (addition.IsChecked.HasValue && addition.IsChecked.Value)
+            {
+                Addition();
+            }
+            else if (addition.IsChecked.HasValue && addition.IsChecked.Value)
+            {
+                Addition();
+            }
+        }
+
+        private void Multiplication()
+        {
+            double leftOperand = double.Parse(lfOperand.Text);
+            double rightOperand = double.Parse(rghOperand.Text);
+            double outcome = 0;
+
+            outcome = leftOperand * rightOperand;
+            expresion.Text = leftOperand + " * " + rightOperand;
+
+            result.Text = outcome.ToString();
+        }
+
+        private void Division()
+        {
+            double leftOperand = double.Parse(lfOperand.Text);
+            double rightOperand = double.Parse(rghOperand.Text);
+            double outcome = 0;
+
+            outcome = leftOperand / rightOperand;
+            expresion.Text = leftOperand + " / " + rightOperand;
+
+            result.Text = outcome.ToString();
+        }
+
+        private void Addition()
+        {
+            double leftOperand = double.Parse(lfOperand.Text);
+            double rightOperand = double.Parse(rghOperand.Text);
+            double outcome = 0;
+
+            outcome = leftOperand + rightOperand;
+            expresion.Text = leftOperand + " + " + rightOperand;
+
+            result.Text = outcome.ToString();
+        }
+
+        private void Subtraction()
+        {
+            double leftOperand = double.Parse(lfOperand.Text);
+            double rightOperand = double.Parse(rghOperand.Text);
+            double outcome = 0;
+
+            outcome = leftOperand - rightOperand;
+            expresion.Text = leftOperand + " - " + rightOperand;
+
+            result.Text = outcome.ToString();
+        }
+
+        private void Pow()
+        {
+            double leftOperand = double.Parse(lfOperand.Text);
+            double rightOperand = double.Parse(rghOperand.Text);
+            double outcome = 0;
+
+            outcome = Math.Pow(leftOperand, rightOperand);
+            expresion.Text = leftOperand + " ^ " + rightOperand;
+
+            result.Text = outcome.ToString();
+        }
+
+        private void Remind()
+        {
+            double leftOperand = double.Parse(lfOperand.Text);
+            double rightOperand = double.Parse(rghOperand.Text);
+            double outcome = 0;
+
+            outcome = leftOperand % rightOperand;
+            expresion.Text = leftOperand + " % " + rightOperand;
+
+            result.Text = outcome.ToString();
+        }
+    }
+}
