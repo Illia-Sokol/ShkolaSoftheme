96b1a399cbcb653cb9dce2cf216391dd35f1f04f
Illia Sokol <illiasokolovskyi@gmail.com>
2018-02-20 02:45:36 +0200

    added task_3 ShapeDescriptor class

---
+++ homeworks/HW7/ShapeDescriptor/ShapeDescriptor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ShapeDescriptor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shape1 = new ShapeDescriptor(2);
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}",
+                shape1._a, shape1._b, shape1._c, shape1._d, shape1._e);
+
+            var shape2 = new ShapeDescriptor(1, 2, 3);
+            Console.WriteLine("{0} {1} {2} {3} {4}",
+                shape2._a, shape2._b, shape2._c, shape2._d, shape2._e);
+        }
+    }
+}
---
+++ homeworks/HW7/ShapeDescriptor/ShapeDescriptor/ShapeDescriptor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeDescriptor
+{
+    class ShapeDescriptor
+    {
+        public int _a { get; set; }
+        public int _b { get; set; }
+        public int _c { get; set; }
+        public int _d { get; set; }
+        public int _e { get; set; }
+
+        public ShapeDescriptor(int a)
+        {
+            _a = _b = _c = _d = _e = a;
+        }
+
+        public ShapeDescriptor(int a, int b, int c)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+
+        public ShapeDescriptor(int a, int b, int c, int e) : this(a, b, 0)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+            _e = e;
+        }
+
+        public void ShapeType()
+        {
+
+        }
+    }
+}
