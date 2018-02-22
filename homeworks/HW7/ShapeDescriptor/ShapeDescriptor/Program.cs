dc3edf703299ad4efe4dbf876cad0339c06a4354
Illia Sokol <illiasokolovskyi@gmail.com>
2018-02-23 00:35:08 +0200

    finished HW7 ShapeDescriptor task

--- homeworks/HW7/ShapeDescriptor/ShapeDescriptor/Program.cs
+++ homeworks/HW7/ShapeDescriptor/ShapeDescriptor/Program.cs
@@ -6,13 +6,21 @@ namespace ShapeDescriptor
     {
         static void Main(string[] args)
         {
-            var shape1 = new ShapeDescriptor(2);
+            var shape1 = new ShapeDescriptor(1);
             Console.WriteLine("{0}, {1}, {2}, {3}, {4}",
-                shape1._a, shape1._b, shape1._c, shape1._d, shape1._e);
+                shape1.A, shape1.B, shape1.C, shape1.D, shape1.E);
 
-            var shape2 = new ShapeDescriptor(1, 2, 3);
+            var shape2 = new ShapeDescriptor(2, 3);
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}",
+                shape2.A, shape2.B, shape2.C, shape2.D, shape2.E);
+
+            var shape3 = new ShapeDescriptor(3, 4, 5);
             Console.WriteLine("{0} {1} {2} {3} {4}",
-                shape2._a, shape2._b, shape2._c, shape2._d, shape2._e);
+                shape3.A, shape3.B, shape3.C, shape3.D, shape3.E);
+
+            var shape4 = new ShapeDescriptor(4, 5, 6, 7);
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}",
+                shape4.A, shape4.B, shape4.C, shape4.D, shape4.E);
         }
     }
 }
--- homeworks/HW7/ShapeDescriptor/ShapeDescriptor/ShapeDescriptor.cs
+++ homeworks/HW7/ShapeDescriptor/ShapeDescriptor/ShapeDescriptor.cs
@@ -6,35 +6,60 @@ namespace ShapeDescriptor
 {
     class ShapeDescriptor
     {
-        public int _a { get; set; }
-        public int _b { get; set; }
-        public int _c { get; set; }
-        public int _d { get; set; }
-        public int _e { get; set; }
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+        public int D { get; set; }
+        public int E { get; set; }
 
         public ShapeDescriptor(int a)
         {
-            _a = _b = _c = _d = _e = a;
+            A = a;
+            ShapeType(A);
         }
 
-        public ShapeDescriptor(int a, int b, int c)
+        public ShapeDescriptor(int a, int b)
         {
-            _a = a;
-            _b = b;
-            _c = c;
+            A = a;
+            B = b;
+            ShapeType(A, B);
         }
 
-        public ShapeDescriptor(int a, int b, int c, int e) : this(a, b, 0)
+        public ShapeDescriptor(int a, int b, int c) : this(a, 0)
         {
-            _a = a;
-            _b = b;
-            _c = c;
-            _e = e;
+            A = a;
+            B = b;
+            C = c;
+            ShapeType(A, B, C);
         }
 
-        public void ShapeType()
+        public ShapeDescriptor(int a, int b, int c, int e) : this(1, b, c)
         {
+            A = a;
+            B = b;
+            C = c;
+            E = e;
+            ShapeType(A, B, C, D);
+        }
 
+        public void ShapeType(int a)
+        {
+            Console.WriteLine("The figure is the square");
+        }
+
+        public void ShapeType(int a, int b)
+        {
+            Console.WriteLine("The figure is the rectangle");
+        }
+
+        public void ShapeType(int a, int b, int c)
+        {
+            Console.WriteLine("The figure is the triangle");
+        }
+
+        public void ShapeType(int a, int b, int c, int e)
+        {
+            Console.WriteLine("The figure is the romb");
         }
     }
 }
