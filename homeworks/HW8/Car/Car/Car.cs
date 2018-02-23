acf63d2a217e8385f06aff9f8bd64e738f903e68
Illia Sokol <illiasokolovskyi@gmail.com>
2018-02-23 09:07:35 +0200

    add HW8 CarConstructor

---
+++ homeworks/HW8/Car/Car/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car
+{
+    class Car
+    {
+        public Engine Engine { get; private set;  }
+        public Color Color { get;  }
+        public Transmission Transmission { get;  }
+
+        public Car(Engine engine, Color color, Transmission transmission)
+        {
+            Console.WriteLine("Create Car");
+        }
+
+        public void Reconstruct(Engine engine)
+        {
+            Engine = engine;
+        }
+    }
+}
