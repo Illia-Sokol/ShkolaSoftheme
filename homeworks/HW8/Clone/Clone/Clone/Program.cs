af4d5db4500d9345bf6d9da8796010ec609bfaae
Illia Sokol <illiasokolovskyi@gmail.com>
2018-02-27 02:18:50 +0200

    copy user by Reference and by Value

---
+++ homeworks/HW8/Clone/Clone/Clone/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Clone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("_________________User Copy by Reference____________");
+            User userCopyByReference = new User("David", "Beckham", 40);
+            Console.WriteLine("{0}, {1}, {2}", 
+                userCopyByReference.Name, userCopyByReference.Surname, userCopyByReference.Age);
+
+            User userCopyByReference2 = userCopyByReference.CopyByReference(userCopyByReference);
+            Console.WriteLine("{0}, {1}, {2}",
+                userCopyByReference2.Name, userCopyByReference2.Surname, userCopyByReference2.Age);
+
+            userCopyByReference.ChangeName("Jack");
+            Console.WriteLine("{0}, {1}, {2}", 
+                userCopyByReference.Name, userCopyByReference.Surname, userCopyByReference.Age);
+            Console.WriteLine("{0}, {1}, {2}",
+                userCopyByReference2.Name, userCopyByReference2.Surname, userCopyByReference2.Age);
+
+            Console.WriteLine("_________________User Copy__________________________");
+
+            User userCopy = new User("David", "Beckham", 40);
+            Console.WriteLine("{0}, {1}, {2}", userCopy.Name, userCopy.Surname, userCopy.Age);
+
+            User userCopy2 = userCopy.Copy(userCopy);
+            Console.WriteLine("{0}, {1}, {2}", userCopy2.Name, userCopy2.Surname, userCopy2.Age);
+
+            userCopy.ChangeName("Jack");
+            Console.WriteLine("{0}, {1}, {2}", userCopy.Name, userCopy.Surname, userCopy.Age);
+            Console.WriteLine("{0}, {1}, {2}", userCopy2.Name, userCopy2.Surname, userCopy2.Age);
+
+        }
+    }
+}
---
+++ homeworks/HW8/Clone/Clone/Clone/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clone
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; }
+        public int Age { get; }
+
+        public User (string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public User Copy (User user)
+        {
+            return new User(user.Name, user.Surname, user.Age);
+        }
+
+        public User CopyByReference(User user)
+        {
+            return user;
+        }
+
+        public void ChangeName (string name)
+        {
+            Name = name;
+        }
+    }
+}
