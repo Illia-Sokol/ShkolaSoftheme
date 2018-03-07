96d1ef81c14e01d68f7eacf7514867789948cbcd
C# <illiasokolovskyi@gmail.com>
2018-03-07 19:30:11 +0200

    added homework 17

---
+++ lessons/17_Collections_Iterator/Demo/02_IteratorBinaryTree/BinaryTree/Tree.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BinaryTree
+{
+    public class Tree<TItem> where TItem : IComparable<TItem>
+    {
+        public TItem NodeData { get; set; }
+        
+        public Tree<TItem> LeftTree { get; set; }
+        
+        public Tree<TItem> RightTree { get; set; }
+
+        public Tree(TItem nodeValue)
+        {
+            NodeData = nodeValue;
+            LeftTree = null;
+            RightTree = null;
+        }
+
+        public void Insert(TItem newItem)
+        {
+            TItem currentNodeValue = NodeData;
+            if (currentNodeValue.CompareTo(newItem) > 0)
+            {
+                // Insert the new item into the left subtree
+                if (LeftTree == null)
+                {
+                    LeftTree = new Tree<TItem>(newItem);
+                }
+                else
+                {
+                    LeftTree.Insert(newItem);
+                }
+            }
+            else
+            {
+                // Insert the new item into the right subtree
+                if (RightTree == null)
+                {
+                    RightTree = new Tree<TItem>(newItem);
+                }
+                else
+                {
+                    RightTree.Insert(newItem);
+                }
+            }
+        }
+
+        public string WalkTree()
+        {
+            string result = "";
+
+            if (LeftTree != null)
+            {
+                result = LeftTree.WalkTree();
+            }
+
+            result += String.Format(" {0} ", NodeData);
+
+            if (RightTree != null)
+            {
+                result += RightTree.WalkTree();
+            }
+
+            return result;
+        }
+
+
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            return new TreeEnumerator<TItem>(this);
+        }
+
+        //IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    return GetEnumerator();
+        //}
+    }
+}
---
+++ lessons/17_Collections_Iterator/Demo/02_IteratorBinaryTree/BinaryTree/TreeEnumerator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTree
+{
+    class TreeEnumerator<TItem> : IEnumerator<TItem> where TItem : IComparable<TItem>
+    {
+        private readonly Tree<TItem> _currentData;
+        
+        private TItem _currentItem;
+        
+        private Queue<TItem> _enumData;
+
+        public TreeEnumerator(Tree<TItem> data)
+        {
+            _currentData = data;
+        }
+
+        private void Populate(Queue<TItem> enumQueue, Tree<TItem> tree)
+        {
+            if (tree.LeftTree != null)
+            {
+                Populate(enumQueue, tree.LeftTree);
+            }
+
+            enumQueue.Enqueue(tree.NodeData);
+
+            if (tree.RightTree != null)
+            {
+                Populate(enumQueue, tree.RightTree);
+            }
+        }
+
+        public TItem Current
+        {
+            get
+            {
+                if (_enumData == null)
+                {
+                    throw new InvalidOperationException("Use MoveNext before calling Current");
+                }
+
+                return _currentItem;
+            }
+        }
+
+        void IDisposable.Dispose()
+        {
+            // throw new NotImplementedException();
+        }
+
+        object System.Collections.IEnumerator.Current
+        {
+            get { return Current; }
+        }
+
+        bool System.Collections.IEnumerator.MoveNext()
+        {
+            if (_enumData == null)
+            {
+                _enumData = new Queue<TItem>();
+                Populate(_enumData, _currentData);
+            }
+
+            if (_enumData.Count > 0)
+            {
+                _currentItem = _enumData.Dequeue();
+                return true;
+            }
+
+            return false;
+        }
+
+        void System.Collections.IEnumerator.Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
