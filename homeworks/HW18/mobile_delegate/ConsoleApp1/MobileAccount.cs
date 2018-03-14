0f00a4c26ab714d4b8b57cfcca8c6dce41f5539b
Illia-Sokol <illiasokolovskyi@gmail.com>
2018-03-14 19:50:44 +0200

    create homework 18 delegate

---
+++ homeworks/HW18/mobile_delegate/ConsoleApp1/MobileAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mobile
+{
+    class CallInfo
+    {
+        public int Sender { get; set; }
+        public int Receiver { get; set; }
+    }
+    class MobileAccount
+    {
+        private readonly int _number;
+
+        public event EventHandler<CallInfo> SendCall ;
+        }
+}
---
+++ homeworks/HW18/mobile_delegate/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mobile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
