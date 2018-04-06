1b9fcc1c174e9a4d9e3f2077673adc165a5d548c
Illia-Sokol <illiasokolovskyi@gmail.com>
2018-04-06 20:57:40 +0300

    added homework httpService

---
+++ lessons/httpService/httpService/httpServiceContract/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Runtime.Serialization;
+
+namespace httpServiceContract
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/EchoGet?value={value}",
+                BodyStyle = WebMessageBodyStyle.Bare,
+                ResponseFormat = WebMessageFormat.Xml)]
+        string EchoGet(string value);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/EchoPost",
+                   BodyStyle = WebMessageBodyStyle.Bare,
+                   ResponseFormat = WebMessageFormat.Xml,
+                   RequestFormat = WebMessageFormat.Xml)]
+        string EchoPost(string value);
+
+    }
+}
---
+++ lessons/httpService/httpService/httpServiceImplementation/Class1.cs
@@ -0,0 +1,18 @@
+﻿
+using httpServiceContract;
+
+namespace httpServiceImplementation
+{
+    public class Service : IService
+    {
+        public string EchoGet(string value)
+        {
+            return "You said " + value;
+        }
+
+        public string EchoPost(string value)
+        {
+            return "You said " + value;
+        }
+    }
+}
